afd9d241cc9380a6526b7b2558e748e64baedcab
Theodore Kimana <kimana.theodore@gmail.com>
2019-01-08 22:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function whisper(string) {
 function logShout(string) {
   console.log('HELLO');
 }
-function logwhisper(string) {
-  console.log("hello");
+function logWhisper(string) {
+  console.log('hello');
 }
